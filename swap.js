93892ea20803bf569a4ee5aecb996a399940b188
Jan De Roeck <jan.deroeck@student.ap.be>
2015-06-18 13:00:57 +0200

    #1 Upload
    18/06/2015

---
+++ swap.js
@@ -0,0 +1,6 @@
+function swap(one, two, three, four) {
+    document.getElementById(one).style.display = 'inline-block';
+    document.getElementById(two).style.display = 'inline-block';
+    document.getElementById(three).style.display = 'none';
+    document.getElementById(four).style.display = 'none';
+}
